e1e6e4551c6a9f1c80294f5a58ee8d1e4312d477
ido sarue <dodojoker67@gmail.com>
2021-12-14 21:47:11 +0200

    added rooms

--- hotel/seed.py
+++ hotel/seed.py
@@ -1,12 +1,25 @@
-
 import os
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel.settings')
 django.setup()
 
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
+from users.models import Room, RoomPrice, RoomType, CustomerPrice
 
-print(get_user_model())
-print(User)
+def create_room_price():
+    for price in [300, 700, 1200]:
+        RoomPrice.objects.create(price=price)
+# create_room_price()
+
+def create_room_type():
+    for index,room_name in enumerate(['suite', 'single', 'double']):
+        RoomType.objects.create(name=room_name, price_per_night_id=index + 1)
+# create_room_type()
+
+def create_rooms():
+    for _ in range(4):
+        for room_type in RoomType.objects.all():
+            Room.objects.create(room_type=room_type)
+# create_rooms()
+
+# CustomerPrice.objects.create()
--- hotel/users/models.py
+++ hotel/users/models.py
@@ -1,49 +1,37 @@
 from django.db import models
+from accounts.models import Profile
+
 
 # Create your models here.
 class Booking(models.Model):
     check_in_date = models.DateField()
     check_out_date = models.DateField()
-    number_of_people = models.ForeignKey('RoomSize', on_delete=models.PROTECT, null=True)
     room = models.ForeignKey('Room', on_delete=models.PROTECT)
-    # user = models.ForeignKey(Profile, on_delete=CASCADE, null=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
 
 
+class CustomerPrice(models.Model):
+    adult_price = models.IntegerField(default=100)
+
+class RoomPrice (models.Model):
+    price = models.IntegerField(default=700)
+
+class Customer(models.Model):
+    age = models.IntegerField(default=18)
+    price_per_night = models.ForeignKey(CustomerPrice, on_delete=models.CASCADE)
 
 class RoomType(models.Model):
     name = models.CharField(max_length=40)
-    def __str__(self):
-        return self.name
-
-class RoomSize(models.Model):
-    max_people = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f'{self.max_people}'
+    price_per_night = models.ForeignKey(RoomPrice, on_delete=models.CASCADE, default=1)
 
 class Room(models.Model):
-    room_size= models.ForeignKey(RoomSize, on_delete=models.PROTECT)
     room_type = models.ForeignKey(RoomType, on_delete=models.PROTECT)
-    price_per_night = models.IntegerField(default=500)
 
-    def __str__(self):
-        return f'{self.room_type.name}'
-
-    def is_vacant(self, start_date, end_date):
-        bookings = self.booking_set.exclude(check_out_date__lt=end_date, check_in_date__lt=start_date)
-        if not bookings.exists():
-            return True
-        else:
-            return False
-        
-    
 class Info(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
     content = models.CharField(max_length=255)
-
-
 
 class Review(models.Model):
     first_name = models.CharField(max_length=40)
